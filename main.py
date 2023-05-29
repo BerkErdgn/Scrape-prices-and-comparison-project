e38ecec01bfc857db4cef2bea490b8f39ae30029
Berk Erdoğan <erdgnberk@gmail.com>
2023-05-29 17:50:58 +0300

    For the third site
    The necessary codes for the third website were written.

--- main.py
+++ main.py
@@ -15,11 +15,89 @@ driver.maximize_window()
 
 
 
+"""
+#--Amazon-- Sorunlu geri dön
+driver.get("https://www.amazon.com.tr/")
+WebDriverWait(driver, 4).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME,"nav-input")))
+search_bar_amazon = driver.find_element(By.CLASS_NAME,"nav-input")
+search_bar_amazon.send_keys(input)
 
+WebDriverWait(driver, 4).until(expected_conditions.visibility_of_element_located((By.ID,"nav-search-submit-button")))
+search_button_amazon = driver.find_element(By.ID,"nav-search-submit-button")
+search_button_amazon.click()
 
+WebDriverWait(driver, 4).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, "a-size-base-plus")))
+item_name_amazon = driver.find_elements(By.CLASS_NAME, "a-size-base-plus")
 
+WebDriverWait(driver, 4).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, "a-size-base-plus")))
+item_price_amazon = driver.find_elements(By.CLASS_NAME, "a-price-whole")
+
+amazon_name_list = []
+amazon_price_list = []
+
+for amazon_name in item_name_amazon:
+    amazon_name_list.append(amazon_name.text)
+
+for amazon_price in item_price_amazon:
+    amazon_price_list.append(amazon_price.text)
+
+print(amazon_name_list)
+print(amazon_price_list)
+print(len(amazon_price_list))
+print(len(amazon_name_list))
+
+df_amazon = pd.DataFrame({'website':"Amazon",'Item Name':amazon_name_list,'Item Price': amazon_price_list })
+
+print(df_amazon)
 
 """
+
+"""
+
+#--ÇicekSepeti--
+driver.get("https://www.ciceksepeti.com/")
+
+WebDriverWait(driver, 4).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME,"item-extra")))
+entry_button_ciceksepeti = driver.find_element(By.CLASS_NAME,"item-extra")
+entry_button_ciceksepeti.click()
+
+WebDriverWait(driver, 4).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME,"product-search__input")))
+search_bar_ciceksepeti = driver.find_element(By.CLASS_NAME,"product-search__input")
+search_bar_ciceksepeti.send_keys(input)
+
+WebDriverWait(driver, 4).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME,"icon-search-444")))
+search_button_ciceksepeti = driver.find_element(By.CLASS_NAME,"icon-search-444")
+search_button_ciceksepeti.click()
+
+WebDriverWait(driver, 4).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, "products__item-title")))
+item_name_ciceksepeti = driver.find_elements(By.CLASS_NAME, "products__item-title")
+
+WebDriverWait(driver, 4).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, "price--now")))
+item_price_ciceksepeti = driver.find_elements(By.CLASS_NAME, "price--now")
+
+ciceksepeti_name_list = []
+ciceksepeti_price_list = []
+
+for ciceksepeti_name in item_name_ciceksepeti:
+    ciceksepeti_name_list.append(ciceksepeti_name.text)
+
+for ciceksepeti_price in item_price_ciceksepeti:
+    price = ciceksepeti_price.text.replace("\n","")
+    ciceksepeti_price_list.append(price)
+
+print(ciceksepeti_name_list)
+print(ciceksepeti_price_list)
+print(len(ciceksepeti_name_list))
+print(len(ciceksepeti_price_list))
+
+df_ciceksepeti = pd.DataFrame({'website':"ÇiçekSepeti",'Item Name':ciceksepeti_name_list,'Item Price': ciceksepeti_price_list })
+
+print(df_ciceksepeti)
+
+
+
+
+
 #--Trendyol--
 driver.get("https://www.trendyol.com/butik/liste/1/kadin")
 WebDriverWait(driver, 4).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME,"V8wbcUhU")))
@@ -51,9 +129,10 @@ print(trenyol_name_list)
 df_trendyol = pd.DataFrame({'website':"Tredyol",'Item Name':trenyol_name_list,'Item Price': trenyol_price_list })
 
 print(df_trendyol)
-"""
 
-"""
+
+
+
 #--HEPSİBURADA--
 
 driver.get("https://www.hepsiburada.com/")
